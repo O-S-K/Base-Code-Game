c81ffffa4bd10b6c0837a136b751ee985caf93e4
Osk <52168556+DevOsk@users.noreply.github.com>
2020-11-29 11:32:26 +0700

    Add files via upload

---
+++ ScrollerTexture.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace OSK
+{
+    public class ScrollerTexture : MonoBehaviour
+    {
+        [Range(-50, 50)]
+        public float speeScroll;
+        public bool isDirX;
+
+        Material _material;
+        float offset;
+
+        void Start()
+        {
+            _material = GetComponent<Renderer>().material;
+        }
+
+        void Update()
+        {
+            offset += (Time.deltaTime * speeScroll) / 10f;
+
+            if (isDirX)
+            {
+                _material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+            }
+            else
+            {
+                _material.SetTextureOffset("_MainTex", new Vector2(0, offset));
+            }
+        }
+    }
+}
