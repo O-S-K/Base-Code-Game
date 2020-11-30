e6ca7b0b3d4ec251582f7e19dd1a2a738aca3fb6
Osk <52168556+DevOsk@users.noreply.github.com>
2020-11-30 15:00:28 +0700

    Open Photoshop

---
+++ OSK/Editor/ActionEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+using System.Diagnostics;
+
+public class ActionEditor : MonoBehaviour
+{
+    [MenuItem("Assets/Open with Photoshop")]
+    private static void GetDataSkill()
+    {
+        Process photoViewer = new Process();
+        photoViewer.StartInfo.FileName = @"D:\Quyle\Pts\PhotoshopCS6Portable.exe";
+        var inf = new FileInfo(AssetDatabase.GetAssetPath(Selection.activeObject));
+        photoViewer.StartInfo.Arguments = inf.FullName;
+        photoViewer.Start();
+    }
+}
