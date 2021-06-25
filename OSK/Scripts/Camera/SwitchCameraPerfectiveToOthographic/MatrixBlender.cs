219b9371b0c5e71245d5d9d0fe2d8b99e33bf7b1
Osk <52168556+O-S-K@users.noreply.github.com>
2021-06-25 19:17:35 +0700

    Scrollview

---
+++ OSK/Scripts/Camera/SwitchCameraPerfectiveToOthographic/MatrixBlender.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Camera))]
+public class MatrixBlender : MonoBehaviour
+{
+    Camera m_camera;
+    private void Start()
+    {
+        m_camera = GetComponent<Camera>();
+    }
+
+    public static Matrix4x4 MatrixLerp(Matrix4x4 from, Matrix4x4 to, float time)
+    {
+        var ret = new Matrix4x4();
+        for (int i = 0; i < 16; i++)
+        {
+            ret[i] = Mathf.Lerp(from[i], to[i], time);
+        }
+        return ret;
+    }
+
+    private IEnumerator LerpFromTo(Matrix4x4 src, Matrix4x4 dest, float duration, float ease, bool reverse)
+    {
+        float startTime = Time.time;
+        while (Time.time - startTime < duration)
+        {
+            float step;
+            if (reverse) step = 1 - Mathf.Pow(1 - (Time.time - startTime) / duration, ease);
+            else step = Mathf.Pow((Time.time - startTime) / duration, ease);
+            m_camera.projectionMatrix = MatrixLerp(src, dest, step);
+            yield return 1;
+        }
+        m_camera.projectionMatrix = dest;
+    }
+
+    public Coroutine BlendToMatrix(Matrix4x4 targetMatrix, float duration, float ease, bool reverse)
+    {
+        StopAllCoroutines();
+        return StartCoroutine(LerpFromTo(m_camera.projectionMatrix, targetMatrix, duration, ease, reverse));
+    }
+}
---
+++ OSK/Scripts/Camera/SwitchCameraPerfectiveToOthographic/PerspectiveSwitcher.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MatrixBlender))]
+public class PerspectiveSwitcher : MonoBehaviour
+{
+    public enum TYPE_CAMERA
+    {
+        Perspective,
+        Orthographic
+    }
+
+    public TYPE_CAMERA typeCamera;
+    public float fov = 60f, orthographicSize = 50f;
+    [Space]
+    public float near = .1f, far = 100f;
+
+    private Matrix4x4 ortho, perspective;
+    private MatrixBlender blender;
+    private Camera m_camera;
+
+    private float aspect;
+    private bool startingCamOthographic;
+
+    void Start()
+    {
+        StartingCamDefaul();
+    }
+
+    void StartingCamDefaul()
+    {
+        aspect = (float)Screen.width / (float)Screen.height;
+
+        float horizontal = orthographicSize * aspect;
+        float verticle = orthographicSize;
+
+        ortho = Matrix4x4.Ortho(-horizontal, horizontal, -verticle, verticle, near, far);
+        perspective = Matrix4x4.Perspective(fov, aspect, near, far);
+        m_camera = GetComponent<Camera>();
+
+        if (typeCamera == TYPE_CAMERA.Orthographic)
+        {
+            m_camera.projectionMatrix = ortho;
+            startingCamOthographic = true;
+        }
+        else if (typeCamera == TYPE_CAMERA.Perspective)
+        {
+            m_camera.projectionMatrix = perspective;
+            startingCamOthographic = false;
+        }
+
+        blender = GetComponent<MatrixBlender>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            startingCamOthographic = !startingCamOthographic;
+            if (startingCamOthographic)
+            {
+                blender.BlendToMatrix(ortho, 1f, 8, true);
+            }
+            else
+            {
+                blender.BlendToMatrix(perspective, 1f, 8, false);
+            }
+        }
+    }
+}
