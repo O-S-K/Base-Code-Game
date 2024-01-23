0b9149a962aa617ab85acf65b2355c1aa0d09e42
Osk <52168556+O-S-K@users.noreply.github.com>
2024-01-23 16:20:36 +0700

    Update ScrollerTexture.cs

--- OSK/Materials/ScrollerTexture.cs
+++ OSK/Materials/ScrollerTexture.cs
@@ -4,30 +4,20 @@ namespace OSK
 {
     public class ScrollerTexture : MonoBehaviour
     {
-        [Range(-50, 50)]
-        public float speeScroll;
-        public bool isDirX;
+         public Vector2 speedScroll;
 
-        Material _material;
-        float offset;
+ 	 private Material _material;
+ 	 private Vector2 offset;
 
-        void Start()
-        {
-            _material = GetComponent<Renderer>().material;
-        }
+	 private void Start()
+ 	 {
+  	    _material = GetComponent<Renderer>().material;
+ 	 }
 
-        void Update()
-        {
-            offset += (Time.deltaTime * speeScroll) / 10f;
-
-            if (isDirX)
-            {
-                _material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
-            }
-            else
-            {
-                _material.SetTextureOffset("_MainTex", new Vector2(0, offset));
-            }
-        }
+ 	 private void Update()
+ 	 {
+  	    offset += (Time.deltaTime * speedScroll) / 10f;
+  	    _material.SetTextureOffset("_MainTex", new Vector2(offset.x, offset.y));
+ 	 }
     }
 }
